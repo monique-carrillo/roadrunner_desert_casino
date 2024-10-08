6d8b18cf955f7e92e0f5325465c26706049e3110
jchicas211 <jchicas@csub.edu>
2024-10-08 08:40:26 -0700

    jchicas commit

---
+++ jchicas.cpp
@@ -0,0 +1,25 @@
+//Josef Chicas
+//Indiv. source file
+//input and display names
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "fonts.h"
+#include "log.h"
+
+
+
+
+int display() {
+
+
+
+
+
+
+
+return 0;
+}
