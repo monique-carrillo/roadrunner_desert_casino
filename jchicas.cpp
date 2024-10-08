620a07c160641c9b5fd56a9f3c356a89e3abab98
jchicas211 <jchicas@csub.edu>
2024-10-08 07:58:55 -0700

    Add files via upload

---
+++ jchicas.cpp
@@ -0,0 +1,24 @@
+//Josef Chicas
+//Indiv. source file
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include <string.h>
+
+
+class name_screen {
+
+private:
+
+
+
+public:
+
+
+
+} screen;
