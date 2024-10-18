d5590d5e420f05f7da804429da3486f0ee7f6c33
DBen-Code <dbenavides4@csub.edu>
2024-10-18 09:11:26 -0700

    Added ;

--- dbenavides.cpp
+++ dbenavides.cpp
@@ -2,8 +2,21 @@
 // Date: 10/08/2024
 // Filename: dbenavides.cpp
 // Purpose: Project Feature
-#include "dbenavides.cpp"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#include <X11/Xlib.h>
+//#include <X11/Xutil.h>
+//#include <GL/gl.h>
+//#include <GL/glu.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "log.h"
+//#include "ppm.h"
+#include "fonts.h"
 using namespace std;
 
 // Obtained Structure from: 
@@ -17,18 +30,18 @@ void background_time() {
 
     if (5 <= hours < 6) {
         // Dawn
-        return "./images/DawnBG.jpg"
+        return "./images/DawnBG.jpg";
     } else if (6 <= hours < 12) {
         // Morning
-        return "./images/MorningBG.jpg"
+        return "./images/MorningBG.jpg";
     } else if (12 <= hours < 17) {
         // Afternoon
-        return "./images/Afternoon.jpg"
+        return "./images/Afternoon.jpg";
     } else if (17 <= hours < 20) {
         // Evening
-        return "./images/EveningBG.webp"
+        return "./images/EveningBG.webp";
     } else if (20 <= hours || hours < 5) {
         // Night
-        return "./images/NightBG.jpg"
+        return "./images/NightBG.jpg";
     }
-};
+}
