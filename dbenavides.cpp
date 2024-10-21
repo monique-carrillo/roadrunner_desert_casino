ff3ce0cf9333750944f6120f43e4b730a3194a8a
DBen-Code <dbenavides4@csub.edu>
2024-10-21 09:40:33 -0700

    Change from Python to C

--- dbenavides.cpp
+++ dbenavides.cpp
@@ -29,16 +29,16 @@ void background_time() {
     // Extract Hours
     int hours = ltime->tm_hour;
 
-    if (5 <= hours < 6) {
+    if (5 <= hours && hours < 6) {
         // Dawn
         return "./images/DawnBG.jpg";
-    } else if (6 <= hours < 12) {
+    } else if (6 <= hours && hours < 12) {
         // Morning
         return "./images/MorningBG.jpg";
-    } else if (12 <= hours < 17) {
+    } else if (12 <= hours && hours < 17) {
         // Afternoon
         return "./images/Afternoon.jpg";
-    } else if (17 <= hours < 20) {
+    } else if (17 <= hours && hours < 20) {
         // Evening
         return "./images/EveningBG.webp";
     } else if (20 <= hours || hours < 5) {
