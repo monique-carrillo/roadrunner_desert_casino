c44d1a0afb153a52b84ae92eaae340a1e42d5d9a
monique-carrillo <moniquecarrillo1994@gmail.com>
2024-12-02 21:09:48 -0800

    slight change

--- mcarrillo.h
+++ mcarrillo.h
@@ -1,10 +1,13 @@
 #ifndef MCARRILLO_H
 #define MCARRILLO_H
 
+void initializeDeck();
+void shuffleDeck();
+//void dealCards(Card *hand, int &handSize, int cardsToDeal, int &deckIndex);
+//int calculateHandValue(Card *hand, int handSize);
+//void displayHand(Card *hand, int handSize);
 void mcarrillo_init();
-void mcarrillo_update();
 void mcarrillo_render();
-void mcarrillo_input(int key);
 void mcarrilloFeature();
 
 #endif // MCARRILLO_H
